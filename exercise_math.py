19c8abcdb2465b2024bdaf18dbd8d31d45ece748
ignaciopiccolo <igpiccolovazquez@mail.austral.edu.ar>
2025-03-28 11:00:08 -0300

    Update exercise_math.py

--- exercise_math.py
+++ exercise_math.py
@@ -1,17 +1,18 @@
-def math():
-a = 57
-b = 7
-suma = a + b
-diferencia = a - b
-producto = a * b
-promedio = (a + b) / 2
-cociente_entero = a // b
-resto = a % b
-division_real = a / b
-print("Suma:", suma)
-print("Diferencia:", diferencia)
-print("Producto:", producto)
-print("Promedio:", promedio)
-print("Cociente entero:", cociente_entero)
-print("Resto de la división entera:", resto)
-print("División real:", division_real)
+def math(): 
+    a = 57
+    b = 7
+    suma = a + b
+    diferencia = a - b
+    producto = a * b
+    promedio = (a + b) / 2
+    cociente_entero = a // b
+    resto = a % b
+    division_real = a / b
+
+    print("Suma:", suma)
+    print("Diferencia:", diferencia)
+    print("Producto:", producto)
+    print("Promedio:", promedio)
+    print("Cociente entero:", cociente_entero)
+    print("Resto de la división entera:", resto)
+    print("División real:", division_real)
